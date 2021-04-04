831970b101ed49e83b3de5853b8a21b4ceafb243
Frank zheng <smldragon@hotmail.com>
2021-04-03 23:33:46 -0400

    maven conversion

--- deploytools/libs.bat
+++ deploytools/libs.bat
@@ -4,9 +4,4 @@ rem this program packs jars in libs folder and sign it.
 rem
 echo on
 call env.bat
-call mvn install -f ../libs_pom_xml
-call pns.bat libs %LIBS.VERSION%
-if %ERRORLEVEL%  NEQ 0 (
-     echo "******* pns.bat libs %LIBS.VERSION% failed ********"
-    exit /B 1
-)
+call mvn install -f ../libs_pom.xml
