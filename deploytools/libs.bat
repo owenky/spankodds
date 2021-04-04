b6d7e26747e3dcf3021955e5bd89d218214bf49d
Frank zheng <smldragon@hotmail.com>
2021-04-03 23:22:52 -0400

    maven conversion

--- deploytools/libs.bat
+++ deploytools/libs.bat
@@ -7,6 +7,6 @@ call env.bat
 call mvn install -f ../libs_pom_xml
 call pns.bat libs %LIBS.VERSION%
 if %ERRORLEVEL%  NEQ 0 (
-     echo "******* pns.bat libs %LIBS.VERSION% Ê§°Ü! ********"
+     echo "******* pns.bat libs %LIBS.VERSION% failed ********"
     exit /B 1
 )
