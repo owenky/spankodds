d212b70db16953caba594c34d65c974688c388f5
Frank zheng <smldragon@hotmail.com>
2021-05-22 17:20:40 -0400

    remove flag isMainTableShown

--- src/main/java/com/sia/client/ui/TableUtils.java
+++ src/main/java/com/sia/client/ui/TableUtils.java
@@ -28,8 +28,8 @@ public abstract class TableUtils {
         tableScrollPane.setHorizontalScrollBarPolicy(ScrollPaneConstants.HORIZONTAL_SCROLLBAR_AS_NEEDED);
         tableScrollPane.getVerticalScrollBar().setUnitIncrement(29);
         tableScrollPane.setRowHeaderView(rowHeaderTable);
-
         tableScrollPane.setCorner(JScrollPane.UPPER_LEFT_CORNER,rowHeaderTable.getTableHeader());
+
         JPanel container = new JPanel();
         container.setLayout(new BorderLayout());
         container.add(tableScrollPane,BorderLayout.CENTER);
