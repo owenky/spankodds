2f4e6befa1cb81ff4b45d76099b4ac4449aebd21
Frank zheng <smldragon@hotmail.com>
2021-05-24 23:38:31 -0400

    enhancement

--- src/main/java/com/sia/client/ui/LinePanel.java
+++ src/main/java/com/sia/client/ui/LinePanel.java
@@ -5,7 +5,6 @@ import com.sia.client.config.Utils;
 import com.sia.client.model.LineData;
 import com.sia.client.model.LinesTableDataSupplier;
 
-import javax.swing.BorderFactory;
 import javax.swing.ImageIcon;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
@@ -16,7 +15,6 @@ import javax.swing.border.MatteBorder;
 import java.awt.Color;
 import java.awt.Font;
 import java.awt.GridLayout;
-import java.awt.LayoutManager;
 
 import static com.sia.client.config.Utils.log;
 
@@ -30,29 +28,32 @@ public class LinePanel extends JPanel {
     private static final Color altcolor = new Color(204, 255, 229);
     private static final Color openercolor = Color.LIGHT_GRAY;
     public static ImageIcon ICON_BLANK = new ImageIcon(Utils.getMediaResource("blank2.gif"));
-    protected JTable table;
     boolean testprint = false;
-    private final JLabel top;
-    private final JLabel bottom;
-    private final JLabel draw;
-    private final JLabel total;
+    final JLabel top;
+    final JLabel bottom;
+    final JLabel draw;
+    final JLabel total;
 
     public LinePanel() {
-        this(new GridLayout(2, 0),SwingConstants.RIGHT);
-        add(top);
-        add(bottom);
-    }
+        this(null);
 
-    public LinePanel(String name) {
-        this(new GridLayout(4, 0),SwingConstants.RIGHT);
-        setName(name);
-        add(top);
-        add(bottom);
-        add(draw);
-        add(total);
     }
-    protected LinePanel(LayoutManager layoutManager,int textAlignment) {
-        super(layoutManager);
+    public LinePanel(String name) {
+        this(SwingConstants.RIGHT);
+        if ( null == name) {
+            setLayout(new GridLayout(2, 0));
+            add(top);
+            add(bottom);
+        } else {
+            setLayout(new GridLayout(4, 0));
+            add(top);
+            add(bottom);
+            add(draw);
+            add(total);
+        }
+        setName(name);
+    }
+    protected LinePanel(int textAlignment) {
         top = new JLabel("", ICON_BLANK, SwingConstants.LEADING);
         bottom = new JLabel("", ICON_BLANK, SwingConstants.LEADING);
         draw = new JLabel("", ICON_BLANK, SwingConstants.LEADING);
@@ -80,16 +81,8 @@ public class LinePanel extends JPanel {
         ToolTipManager.sharedInstance().setDismissDelay(60000);
 
     }
-    public void setSoccerLines(SoccerSpreadTotalView stv, int row, int col) {
+    public void setSoccerLines(JTable table,SoccerSpreadTotalView stv, int row, int col) {
 
-        top.setIconTextGap(1);
-        bottom.setIconTextGap(1);
-        draw.setIconTextGap(1);
-        total.setIconTextGap(1);
-        top.setHorizontalAlignment(SwingConstants.RIGHT);
-        bottom.setHorizontalAlignment(SwingConstants.RIGHT);
-        draw.setHorizontalAlignment(SwingConstants.RIGHT);
-        total.setHorizontalAlignment(SwingConstants.RIGHT);
         LineData[] boxes;
         try {
 
@@ -116,13 +109,13 @@ public class LinePanel extends JPanel {
             } else {
                 this.setToolTipText(null);
             }
-            setTop(boxes[0], row, col);
+            setTop(table,boxes[0], row, col);
 
-            setBottom(boxes[1], row, col);
+            setBottom(table,boxes[1], row, col);
 
-            setDraw(boxes[2], row, col);
+            setDraw(table,boxes[2], row, col);
 
-            setTotal(boxes[3], row, col);
+            setTotal(table,boxes[3], row, col);
 
         } catch (Exception ex) {
             log("Error=" + "..." + stv + "..row=" + row + "...col=" + col);
@@ -130,7 +123,7 @@ public class LinePanel extends JPanel {
         }
     }
 
-    public void setTop(LineData ld, int row, int col) {
+    public void setTop(JTable table,LineData ld, int row, int col) {
         try {
             boolean blackorred = false;
             top.setText(ld.getData());
@@ -205,7 +198,7 @@ public class LinePanel extends JPanel {
 
     }
 
-    public void setBottom(LineData ld, int row, int col) {
+    public void setBottom(JTable table,LineData ld, int row, int col) {
 
         boolean blackorred = false;
         bottom.setText(ld.getData());
@@ -271,7 +264,7 @@ public class LinePanel extends JPanel {
 
     }
 
-    private void setDraw(LineData ld, int row, int col) {
+    private void setDraw(JTable table,LineData ld, int row, int col) {
 
         boolean blackorred = false;
         draw.setText(ld.getData());
@@ -339,7 +332,7 @@ public class LinePanel extends JPanel {
         }
     }
 
-    private void setTotal(LineData ld, int row, int col) {
+    private void setTotal(JTable table,LineData ld, int row, int col) {
 
         boolean blackorred = false;
         total.setText(ld.getData());
@@ -407,11 +400,7 @@ public class LinePanel extends JPanel {
 
     }
 
-    public void setLines(SpreadTotalView stv, int row, int col) {
-        top.setIconTextGap(1);
-        bottom.setIconTextGap(1);
-        top.setHorizontalAlignment(SwingConstants.RIGHT);
-        bottom.setHorizontalAlignment(SwingConstants.RIGHT);
+    public void setLines(JTable table,SpreadTotalView stv, int row, int col) {
         LineData[] boxes;
         try {
 
@@ -437,8 +426,8 @@ public class LinePanel extends JPanel {
                 this.setToolTipText(null);
             }
 
-            setTop(boxes[0], row, col);
-            setBottom(boxes[1], row, col);
+            setTop(table,boxes[0], row, col);
+            setBottom(table,boxes[1], row, col);
         } catch (Exception ex) {
             log("ERROR=..." + stv + "..row=" + row + "...col=" + col);
             log(ex);
@@ -446,22 +435,11 @@ public class LinePanel extends JPanel {
 
     }
 
-    public void setTeams(TeamView stv, int row, int col) {
-
-        top.setHorizontalAlignment(SwingConstants.LEFT);
-        bottom.setHorizontalAlignment(SwingConstants.LEFT);
-
-        top.setHorizontalTextPosition(SwingConstants.RIGHT);
-        bottom.setHorizontalTextPosition(SwingConstants.RIGHT);
-
-        draw.setHorizontalAlignment(SwingConstants.LEFT);
-        total.setHorizontalAlignment(SwingConstants.LEFT);
-        draw.setHorizontalTextPosition(SwingConstants.RIGHT);
-        total.setHorizontalTextPosition(SwingConstants.RIGHT);
+    public void setTeams(JTable table,TeamView stv, int row, int col) {
 
         LineData[] boxes = stv.getCurrentBoxes();
-        setTop(boxes[0], row, col);
-        setBottom(boxes[1], row, col);
+        setTop(table,boxes[0], row, col);
+        setBottom(table,boxes[1], row, col);
         String display = "";
         if (isSoccer()) {
             try {
@@ -473,13 +451,13 @@ public class LinePanel extends JPanel {
             if (display.equalsIgnoreCase("SpreadTotal")) {
                 LineData draw = new LineData(ICON_BLANK, "Over", Color.WHITE);
                 LineData total = new LineData(ICON_BLANK, "Under", Color.WHITE);
-                setDraw(draw, row, col);
-                setTotal(total, row, col);
+                setDraw(table,draw, row, col);
+                setTotal(table,total, row, col);
             } else {
                 LineData draw = new LineData(ICON_BLANK, "Draw", Color.WHITE);
                 LineData total = new LineData(ICON_BLANK, "Total", Color.WHITE);
-                setDraw(draw, row, col);
-                setTotal(total, row, col);
+                setDraw(table,draw, row, col);
+                setTotal(table,total, row, col);
             }
 
 
@@ -497,25 +475,18 @@ public class LinePanel extends JPanel {
         return SiaConst.SoccerStr.equalsIgnoreCase(getName());
     }
 
-    public void setTime(TimeView stv, int row, int col) {
-
-        top.setHorizontalAlignment(SwingConstants.CENTER);
-        bottom.setHorizontalAlignment(SwingConstants.CENTER);
+    public void setTime(JTable table,TimeView stv, int row, int col) {
         LineData[] boxes = stv.getCurrentBoxes();
-        setTop(boxes[0], row, col);
-        setBottom(boxes[1], row, col);
+        setTop(table,boxes[0], row, col);
+        setBottom(table,boxes[1], row, col);
         if (isSoccer()) {
             LineData blank = new LineData(ICON_BLANK, "", Color.WHITE);
-            setDraw(blank, row, col);
-            setTotal(blank, row, col);
+            setDraw(table,blank, row, col);
+            setTotal(table,blank, row, col);
         }
     }
 
-    public void setChart(ChartView stv, int row, int col) {
-
-        top.setHorizontalAlignment(SwingConstants.CENTER);
-        bottom.setHorizontalAlignment(SwingConstants.CENTER);
-
+    public void setChart(JTable table,ChartView stv, int row, int col) {
         try {
             LinesTableData ltd = ((LinesTableDataSupplier) table).getLinesTableData(row);
 
@@ -527,22 +498,16 @@ public class LinePanel extends JPanel {
 
 
         LineData[] boxes = stv.getCurrentBoxes();
-        setTop(boxes[0], row, col);
-        setBottom(boxes[1], row, col);
+        setTop(table,boxes[0], row, col);
+        setBottom(table,boxes[1], row, col);
         if (isSoccer()) {
             LineData blank = new LineData(ICON_BLANK, "", Color.WHITE);
-            setDraw(blank, row, col);
-            setTotal(blank, row, col);
+            setDraw(table,blank, row, col);
+            setTotal(table,blank, row, col);
         }
     }
 
-    public void setSoccerChart(SoccerChartView stv, int row, int col) {
-        top.setHorizontalAlignment(SwingConstants.CENTER);
-        bottom.setHorizontalAlignment(SwingConstants.CENTER);
-        draw.setHorizontalAlignment(SwingConstants.CENTER);
-        total.setHorizontalAlignment(SwingConstants.CENTER);
-
-
+    public void setSoccerChart(JTable table,SoccerChartView stv, int row, int col) {
         try {
             LinesTableData ltd = ((LinesTableDataSupplier) table).getLinesTableData(row);
             stv.setDisplayType(ltd.getDisplayType());
@@ -552,51 +517,40 @@ public class LinePanel extends JPanel {
         }
 
         LineData[] boxes = stv.getCurrentBoxes();
-        setTop(boxes[0], row, col);
-        setBottom(boxes[1], row, col);
-        setDraw(boxes[2], row, col);
-        setTotal(boxes[3], row, col);
+        setTop(table,boxes[0], row, col);
+        setBottom(table,boxes[1], row, col);
+        setDraw(table,boxes[2], row, col);
+        setTotal(table,boxes[3], row, col);
     }
 
-    public void setHeader(HeaderView stv, int row, int col) {
-        top.setHorizontalAlignment(SwingConstants.LEFT);
-        bottom.setHorizontalAlignment(SwingConstants.LEFT);
+    public void setHeader(JTable table,HeaderView stv, int row, int col) {
         LineData[] boxes = stv.getCurrentBoxes();
-        setTop(boxes[0], row, col);
-        setBottom(boxes[1], row, col);
+        setTop(table,boxes[0], row, col);
+        setBottom(table,boxes[1], row, col);
     }
 
-    public void setGamenumbers(GameNumberView stv, int row, int col) {
-        top.setHorizontalAlignment(SwingConstants.CENTER);
-        bottom.setHorizontalAlignment(SwingConstants.CENTER);
+    public void setGamenumbers(JTable table,GameNumberView stv, int row, int col) {
         LineData[] boxes = stv.getCurrentBoxes();
-        setTop(boxes[0], row, col);
-        setBottom(boxes[1], row, col);
+        setTop(table,boxes[0], row, col);
+        setBottom(table,boxes[1], row, col);
     }
 
-    public void setSoccerGamenumbers(SoccerGameNumberView stv, int row, int col) {
-        top.setHorizontalAlignment(SwingConstants.CENTER);
-        bottom.setHorizontalAlignment(SwingConstants.CENTER);
+    public void setSoccerGamenumbers(JTable table,SoccerGameNumberView stv, int row, int col) {
         LineData[] boxes = stv.getCurrentBoxes();
-        setTop(boxes[0], row, col);
-        setBottom(boxes[1], row, col);
-        setDraw(boxes[2], row, col);
-        setTotal(boxes[3], row, col);
+        setTop(table,boxes[0], row, col);
+        setBottom(table,boxes[1], row, col);
+        setDraw(table,boxes[2], row, col);
+        setTotal(table,boxes[3], row, col);
     }
 
-    public void setInfo(InfoView stv, int row, int col) {
-
-
-        top.setHorizontalAlignment(SwingConstants.LEFT);
-        top.setBorder(BorderFactory.createMatteBorder(-1, -1, 1, -1, Color.BLACK));
-        bottom.setHorizontalAlignment(SwingConstants.LEFT);
+    public void setInfo(JTable table,InfoView stv, int row, int col) {
         LineData[] boxes = stv.getCurrentBoxes();
-        setTop(boxes[0], row, col);
-        setBottom(boxes[1], row, col);
+        setTop(table,boxes[0], row, col);
+        setBottom(table,boxes[1], row, col);
         if (isSoccer()) {
             LineData blank = new LineData(ICON_BLANK, "", Color.WHITE);
-            setDraw(blank, row, col);
-            setTotal(blank, row, col);
+            setDraw(table,blank, row, col);
+            setTotal(table,blank, row, col);
         }
 
     }
--- src/main/java/com/sia/client/ui/LineRenderer.java
+++ src/main/java/com/sia/client/ui/LineRenderer.java
@@ -1,20 +1,127 @@
 package com.sia.client.ui;
 
+import javax.swing.BorderFactory;
 import javax.swing.JTable;
+import javax.swing.SwingConstants;
 import javax.swing.table.TableCellRenderer;
+import java.awt.Color;
 import java.awt.Component;
 
 import static com.sia.client.config.Utils.log;
 
-public class LineRenderer extends LinePanel implements TableCellRenderer {
+public class LineRenderer implements TableCellRenderer {
+
+    private final LinePanel teamLinePanel;
+    private final LinePanel spreadTotalPanel;
+    private final LinePanel gameNumbersPanel;
+    private final LinePanel soccerNumbersPanel;
+    private final LinePanel soccerLinePanel;
+    private final LinePanel timePanel;
+    private final LinePanel chartPanel;
+    private final LinePanel soccerChartPanel;
+    private final LinePanel infoPanel;
+    private final LinePanel headerPanel;
 
     public LineRenderer() {
+        this(null);
     }
 
     public LineRenderer(String name) {
-        super(name);
+        teamLinePanel = createTeamLinePanel(name);
+        spreadTotalPanel = createSpreadTotalPanel(name);
+        gameNumbersPanel = createGameNumbersPanel(name);
+        soccerNumbersPanel = createSoccerNumbersPanel(name);
+        soccerLinePanel = createSoccerLinePanel(name);
+        timePanel = createTimePanel(name);
+        chartPanel = createChartPanel(name);
+        soccerChartPanel = createSoccerChartPanel(name);
+        infoPanel = createInfoPanel(name);
+        headerPanel = createHeaderPanel(name);
     }
+    private LinePanel createSoccerLinePanel(String name) {
+        LinePanel lp = new LinePanel(name);
+        lp.top.setIconTextGap(1);
+        lp.bottom.setIconTextGap(1);
+        lp.draw.setIconTextGap(1);
+        lp.total.setIconTextGap(1);
+        lp.top.setHorizontalAlignment(SwingConstants.RIGHT);
+        lp.bottom.setHorizontalAlignment(SwingConstants.RIGHT);
+        lp.draw.setHorizontalAlignment(SwingConstants.RIGHT);
+        lp.total.setHorizontalAlignment(SwingConstants.RIGHT);
 
+        return lp;
+    }
+    private LinePanel createHeaderPanel(String name) {
+        LinePanel lp = new LinePanel(name);
+        lp.top.setHorizontalAlignment(SwingConstants.LEFT);
+        lp.bottom.setHorizontalAlignment(SwingConstants.LEFT);
+
+        return lp;
+    }
+    private LinePanel createInfoPanel(String name) {
+        LinePanel lp = new LinePanel(name);
+        lp.top.setHorizontalAlignment(SwingConstants.LEFT);
+        lp.top.setBorder(BorderFactory.createMatteBorder(-1, -1, 1, -1, Color.BLACK));
+        lp.bottom.setHorizontalAlignment(SwingConstants.LEFT);
+
+        return lp;
+    }
+    private LinePanel createSoccerChartPanel(String name) {
+        LinePanel lp = new LinePanel(name);
+        lp.top.setHorizontalAlignment(SwingConstants.CENTER);
+        lp.bottom.setHorizontalAlignment(SwingConstants.CENTER);
+        lp.draw.setHorizontalAlignment(SwingConstants.CENTER);
+        lp.total.setHorizontalAlignment(SwingConstants.CENTER);
+
+        return lp;
+    }
+    private LinePanel createChartPanel(String name) {
+        LinePanel lp = new LinePanel(name);
+        lp.top.setHorizontalAlignment(SwingConstants.CENTER);
+        lp.bottom.setHorizontalAlignment(SwingConstants.CENTER);
+
+        return lp;
+    }
+    private LinePanel createTimePanel(String name) {
+        LinePanel lp = new LinePanel(name);
+        lp.top.setHorizontalAlignment(SwingConstants.CENTER);
+        lp.bottom.setHorizontalAlignment(SwingConstants.CENTER);
+        return lp;
+    }
+    private LinePanel createSoccerNumbersPanel(String name) {
+        LinePanel lp = new LinePanel(name);
+        lp.top.setHorizontalAlignment(SwingConstants.CENTER);
+        lp.bottom.setHorizontalAlignment(SwingConstants.CENTER);
+        return lp;
+    }
+    private LinePanel createGameNumbersPanel(String name) {
+        LinePanel lp = new LinePanel(name);
+        lp.top.setHorizontalAlignment(SwingConstants.CENTER);
+        lp.bottom.setHorizontalAlignment(SwingConstants.CENTER);
+        return lp;
+    }
+    private LinePanel createSpreadTotalPanel(String name) {
+        LinePanel lp = new LinePanel(name);
+        lp.top.setIconTextGap(1);
+        lp.bottom.setIconTextGap(1);
+        lp.top.setHorizontalAlignment(SwingConstants.RIGHT);
+        lp.bottom.setHorizontalAlignment(SwingConstants.RIGHT);
+        return lp;
+    }
+    private LinePanel createTeamLinePanel(String name) {
+        LinePanel lp = new LinePanel(name);
+        lp.top.setHorizontalAlignment(SwingConstants.LEFT);
+        lp.bottom.setHorizontalAlignment(SwingConstants.LEFT);
+
+        lp.top.setHorizontalTextPosition(SwingConstants.RIGHT);
+        lp.bottom.setHorizontalTextPosition(SwingConstants.RIGHT);
+
+        lp.draw.setHorizontalAlignment(SwingConstants.LEFT);
+        lp.total.setHorizontalAlignment(SwingConstants.LEFT);
+        lp.draw.setHorizontalTextPosition(SwingConstants.RIGHT);
+        lp.total.setHorizontalTextPosition(SwingConstants.RIGHT);
+        return lp;
+    }
     @Override
     public Component getTableCellRendererComponent(JTable table, Object value, boolean isSelected, boolean hasFocus, int row, int column) {
         // here value will be infoview,teamview,gmnumview,timeview,or spreadtotalview....
@@ -27,31 +134,40 @@ public class LineRenderer extends LinePanel implements TableCellRenderer {
             }
             return null;
         }
-
-        this.table = table;
+        LinePanel rtn;
         if (value instanceof SpreadTotalView) {
-            setLines((SpreadTotalView) value, row, column);
+            spreadTotalPanel.setLines(table,(SpreadTotalView) value, row, column);
+            rtn = spreadTotalPanel;
         } else if (value instanceof SoccerSpreadTotalView) {
-            setSoccerLines((SoccerSpreadTotalView) value, row, column);
+            soccerLinePanel.setSoccerLines(table,(SoccerSpreadTotalView) value, row, column);
+            rtn = soccerLinePanel;
         } else if (value instanceof TeamView) {
-            setTeams((TeamView) value, row, column);
+            teamLinePanel.setTeams(table,(TeamView) value, row, column);
+            rtn = teamLinePanel;
         } else if (value instanceof GameNumberView) {
-            setGamenumbers((GameNumberView) value, row, column);
+            gameNumbersPanel.setGamenumbers(table,(GameNumberView) value, row, column);
+            rtn = gameNumbersPanel;
         } else if (value instanceof SoccerGameNumberView) {
-
-            setSoccerGamenumbers((SoccerGameNumberView) value, row, column);
-
+            soccerNumbersPanel.setSoccerGamenumbers(table,(SoccerGameNumberView) value, row, column);
+            rtn = soccerNumbersPanel;
         } else if (value instanceof TimeView) {
-            setTime((TimeView) value, row, column);
+            timePanel.setTime(table,(TimeView) value, row, column);
+            rtn = timePanel;
         } else if (value instanceof ChartView) {
-            setChart((ChartView) value, row, column);
+            chartPanel.setChart(table,(ChartView) value, row, column);
+            rtn = chartPanel;
         } else if (value instanceof SoccerChartView) {
-            setSoccerChart((SoccerChartView) value, row, column);
+            soccerChartPanel.setSoccerChart(table,(SoccerChartView) value, row, column);
+            rtn = soccerChartPanel;
         } else if (value instanceof InfoView) {
-            setInfo((InfoView) value, row, column);
+            infoPanel.setInfo(table,(InfoView) value, row, column);
+            rtn = infoPanel;
         } else if (value instanceof HeaderView) {
-            setHeader((HeaderView) value, row, column);
+            headerPanel.setHeader(table,(HeaderView) value, row, column);
+            rtn = headerPanel;
+        } else {
+            throw new IllegalStateException("Illegal value:"+value);
         }
-        return this;
+        return rtn;
     }
 }
