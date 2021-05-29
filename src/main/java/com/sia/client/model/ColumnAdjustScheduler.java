8fff0e3babc0221eb685754384e1dee14fffddb3
Frank zheng <smldragon@hotmail.com>
2021-05-29 11:21:29 -0400

    improved pooling

--- src/main/java/com/sia/client/model/ColumnAdjustScheduler.java
+++ src/main/java/com/sia/client/model/ColumnAdjustScheduler.java
@@ -3,54 +3,34 @@ package com.sia.client.model;
 import com.sia.client.config.Utils;
 import com.sia.client.ui.ColumnCustomizableTable;
 
-import java.util.ArrayList;
-import java.util.Collection;
 import java.util.List;
 import java.util.Map;
 import java.util.Set;
-import java.util.concurrent.ArrayBlockingQueue;
-import java.util.concurrent.Executors;
-import java.util.concurrent.ScheduledExecutorService;
-import java.util.concurrent.TimeUnit;
-import java.util.concurrent.atomic.AtomicBoolean;
+import java.util.function.Consumer;
 import java.util.stream.Collectors;
-
-import static com.sia.client.config.Utils.log;
 
 public class ColumnAdjustScheduler {
 
-    private static final long DefaultUpdatePeriodInMilliSeconds = 3000L;
-    private final ArrayBlockingQueue<TableModelRowData> rowDataQueue = new  ArrayBlockingQueue<>(10000);
-    private final ScheduledExecutorService excutorService = Executors.newSingleThreadScheduledExecutor();
-    private long updatePeriodInMilliSeconds;
-    private final AtomicBoolean isStarted = new AtomicBoolean(false);
-
+    private final MessageConsumingScheduler<TableModelRowData> delegator;
     public ColumnAdjustScheduler() {
-        updatePeriodInMilliSeconds = DefaultUpdatePeriodInMilliSeconds;
-    }
-    public void setUpdatePeriodInMilliSeconds(long updatePeriodInMilliSeconds){
-        this.updatePeriodInMilliSeconds = updatePeriodInMilliSeconds;
+        delegator = new MessageConsumingScheduler<>(messageConsumer());
+        //this scheduler is ONLY called from GameMessageProcessor::createConsumer, which pools message id. Don't need another layer of pooling so set update period to -1 to disable scheduling and process request immediately. 05/29/2021
+        delegator.setUpdatePeriodInMilliSeconds(-1L);
     }
     public void addRowData(TableModelRowData rowData) {
-        if ( isStarted.compareAndSet(false,true)) {
-            excutorService.scheduleAtFixedRate(this::executeGameBatch,0, updatePeriodInMilliSeconds, TimeUnit.MICROSECONDS);
-        }
-        try {
-            rowDataQueue.put(rowData);
-        } catch (InterruptedException e) {
-            log(e);
-        }
+        delegator.addMessage(rowData);
     }
-    private void executeGameBatch() {
-        Collection<TableModelRowData> buffer = new ArrayList<>();
-        rowDataQueue.drainTo(buffer);
-        Map<ColumnCustomizableTable, List<TableModelRowData>> map = buffer.stream().distinct()
-                .collect(Collectors.groupingBy(TableModelRowData::getTable));
-        map.keySet().forEach((mainTable)-> {
-            List<TableModelRowData> structs = map.get(mainTable);
-            Set<Integer> gameIdSet = structs.stream().map(TableModelRowData::getRowModelIndex).collect(Collectors.toSet());
-            Integer [] rowIndexArr = gameIdSet.toArray(new Integer[0]);
-            Utils.checkAndRunInEDT( () ->mainTable.adjustColumnsOnRows(rowIndexArr));
-        });
+    private Consumer<List<TableModelRowData>> messageConsumer() {
+
+        return (buffer)-> {
+            Map<ColumnCustomizableTable<?>, List<TableModelRowData>> map = buffer.stream().distinct()
+                    .collect(Collectors.groupingBy(TableModelRowData::getTable));
+            map.keySet().forEach((mainTable)-> {
+                List<TableModelRowData> structs = map.get(mainTable);
+                Set<Integer> gameIdSet = structs.stream().map(TableModelRowData::getRowModelIndex).collect(Collectors.toSet());
+                Integer [] rowIndexArr = gameIdSet.toArray(new Integer[0]);
+                Utils.checkAndRunInEDT( () ->mainTable.adjustColumnsOnRows(rowIndexArr));
+            });
+        };
     }
 }
