6256b8c27115494ab11216d6a301fd956b1db528
Frank zheng <smldragon@hotmail.com>
2021-05-28 23:22:11 -0400

    abstract MainScreen

--- src/test/java/com/sia/client/ui/simulator/MainScreenTest.java
+++ src/test/java/com/sia/client/ui/simulator/MainScreenTest.java
@@ -1,17 +1,18 @@
 package com.sia.client.ui.simulator;
 
-import com.sia.client.model.AbstratScreen;
+import com.sia.client.model.AbstractScreen;
 import com.sia.client.ui.ColumnCustomizableTable;
 
 import javax.swing.JPanel;
 
-public class MainScreenTest extends JPanel implements AbstratScreen<TestGame> {
+public class MainScreenTest extends JPanel implements AbstractScreen<TestGame> {
 
     private final TableProperties tblProp;
     public MainScreenTest(TableProperties tblProp) {
         setName("Main screen of table "+tblProp.table.getName());
         this.tblProp = tblProp;
     }
+    @Override
     public void destroyMe() {
 
     }
