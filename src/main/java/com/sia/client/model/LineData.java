f6f63d836b4f4ba2ac446a943fc2644a6e62e807
Frank zheng <smldragon@hotmail.com>
2021-06-01 19:34:27 -0400

    reformat

--- src/main/java/com/sia/client/model/LineData.java
+++ src/main/java/com/sia/client/model/LineData.java
@@ -5,73 +5,67 @@ import java.awt.Color;
 
 public class LineData {
 
-  
-  public ImageIcon  m_icon;
-  public String m_data;
-  public Color m_color;
-  public String border = "";
-  public String tooltip = "";
 
-  public LineData(ImageIcon icon, String data,Color c) 
-  {
-    m_icon = icon;
-    m_data = data;
-	m_color = c;
-  }
-  
-  public LineData(ImageIcon icon, String data,Color c,String border) 
-  {
-    m_icon = icon;
-    m_data = data;
-	m_color = c;
-	this.border = border;
-  }
+    public ImageIcon m_icon;
+    public String m_data;
+    public Color m_color;
+    public String border = "";
+    public String tooltip = "";
 
-  public String toString() 
-	{
-    return m_data.toString();
-	}
-  
-  public ImageIcon getIcon()
-	{
-	  return m_icon;
-	}
-    public String getData()
-	{
-	    return m_data;
-	}
-    public String getBorder()
-	{
-	    return border;
-	}	
-    public String getTooltip()
-	{
-	    return tooltip;
-	}	 
-    public void setIcon(ImageIcon i)
-	{
-		m_icon = i;
-	}
-    public void setData(String s)
-	{
-	    m_data = s;
-	}
-	public void setBorder(String s)
-	{
-	    border = s;
-	}
-	public void setTooltip(String s)
-	{
-	    tooltip = s;
-	}
-    public void setBackgroundColor(Color i)
-	{
-		m_color = i;
-	}
-   public Color getBackgroundColor()
-	{
-		return m_color;
-	}	
- 
+    public LineData(ImageIcon icon, String data, Color c) {
+        m_icon = icon;
+        m_data = data;
+        m_color = c;
+    }
+
+    public LineData(ImageIcon icon, String data, Color c, String border) {
+        this(icon,data,c);
+        this.border = border;
+    }
+
+    public String toString() {
+        return m_data.toString();
+    }
+
+    public ImageIcon getIcon() {
+        return m_icon;
+    }
+
+    public void setIcon(ImageIcon i) {
+        m_icon = i;
+    }
+
+    public String getData() {
+        return m_data;
+    }
+
+    public void setData(String s) {
+        m_data = s;
+    }
+
+    public String getBorder() {
+        return border;
+    }
+
+    public void setBorder(String s) {
+        border = s;
+    }
+
+    public String getTooltip() {
+        return tooltip;
+    }
+
+    public void setTooltip(String s) {
+        tooltip = s;
+    }
+
+    public Color getBackgroundColor() {
+        return m_color;
+    }
+
+    public void setBackgroundColor(Color i) {
+        m_color = i;
+    }
+
 
 }
