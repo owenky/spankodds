7a72728fbdf91c71d840f1aab6ab959cb2761ab3
Frank zheng <smldragon@hotmail.com>
2021-06-12 00:37:37 -0400

    center header

--- src/main/java/com/sia/client/ui/ColumnHeaderDrawer.java
+++ src/main/java/com/sia/client/ui/ColumnHeaderDrawer.java
@@ -2,6 +2,7 @@ package com.sia.client.ui;
 
 import com.sia.client.model.ColumnHeaderProvider;
 import com.sia.client.model.KeyedObject;
+import sun.swing.SwingUtilities2;
 
 import javax.swing.JComponent;
 import javax.swing.JFrame;
@@ -12,6 +13,7 @@ import javax.swing.SwingUtilities;
 import java.awt.BorderLayout;
 import java.awt.Color;
 import java.awt.Font;
+import java.awt.FontMetrics;
 import java.awt.Rectangle;
 import java.util.HashMap;
 import java.util.Map;
@@ -26,9 +28,10 @@ public class ColumnHeaderDrawer<V extends KeyedObject> {
     }
     public void drawOnViewIndex(int rowViewIndex, Object headerValue,int horizontalScrollBarAdjustmentValue) {
         ColumnHeaderProvider<V> columnHeaderProvider = mainTable.getModel().getColumnHeaderProvider();
+        Font headerFont = columnHeaderProvider.getHeaderFont();
         JComponent headerComponent = columnHeaderComponentMap.computeIfAbsent(String.valueOf(headerValue), header -> makeColumnHeaderComp(mainTable, header,columnHeaderProvider.getHeaderForeground()
-                , columnHeaderProvider.getHeaderFont()));
-        layOutColumnHeader(rowViewIndex, mainTable, headerComponent, columnHeaderProvider.getColumnHeaderHeight(), horizontalScrollBarAdjustmentValue);
+                ,headerFont));
+        layOutColumnHeader(rowViewIndex, mainTable, headerComponent, String.valueOf(headerValue),columnHeaderProvider.getColumnHeaderHeight(), horizontalScrollBarAdjustmentValue,headerFont);
     }
     private static <V extends KeyedObject> JComponent makeColumnHeaderComp(ColumnCustomizableTable<V> jtable, String gameGroupHeader, Color headerForeGround, Font titleFont) {
         JPanel jPanel = new JPanel();
@@ -46,28 +49,32 @@ public class ColumnHeaderDrawer<V extends KeyedObject> {
         return jPanel;
     }
 
-    private static <V extends KeyedObject> void layOutColumnHeader(int rowViewIndex, ColumnCustomizableTable<V> mainTable, JComponent header, int headerHeight, int diffByScroll) {
+    private static <V extends KeyedObject> void layOutColumnHeader(int rowViewIndex, ColumnCustomizableTable<V> mainTable, JComponent header, String headerStr,int headerHeight, int diffByScroll, Font titleFont) {
         if ( 0 > diffByScroll ) {
             diffByScroll = 0;
         }
         JFrame topFrame = (JFrame) SwingUtilities.getWindowAncestor(mainTable);
         Rectangle r1 = mainTable.getCellRect(rowViewIndex-1, 0, true);
+        int stringWidth = getStringWidth(header,titleFont,headerStr);
 //        int x1 = 5; //keep leading space from last locked column
         int x1 = (int)topFrame.getSize().getWidth()/2 - mainTable.getRowHeaderTable().getWidth();
-System.out.println("window half width = "+(int)topFrame.getSize().getWidth()/2+", row header width="+ mainTable.getRowHeaderTable().getWidth()+", x1=================="+x1);
+        x1 = x1 - stringWidth/2;
+        int x2;
         if ( x1 < 0) {
-            x1 = (int)topFrame.getSize().getWidth()/2;
+            x2 = (mainTable.getRowHeaderTable().getWidth() - stringWidth)/2;
             mainTable.remove(header);
             mainTable.getRowHeaderTable().add(header);
         } else {
+            x2 = x1;
             mainTable.getRowHeaderTable().remove(header);
             mainTable.add(header);
         }
         int y1 = (int) (r1.getY() + r1.getHeight());
         int width = (int)header.getPreferredSize().getWidth();
-        header.setBounds(x1+diffByScroll, y1, width, headerHeight);
+        header.setBounds(x2+diffByScroll, y1, width, headerHeight);
     }
-//    private static int getStringWidth(Font font, String text) {
-//
-//    }
+    private static int getStringWidth(JComponent jComponent,Font font, String text) {
+        FontMetrics fm = jComponent.getFontMetrics(font);
+        return SwingUtilities2.stringWidth(jComponent, fm, text);
+    }
 }
