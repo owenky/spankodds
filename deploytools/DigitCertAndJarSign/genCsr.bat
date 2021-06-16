a02a18ba3ebc50d135d2078df6e8c39dfbb65316
Frank zheng <smldragon@hotmail.com>
2021-06-16 12:59:27 -0400

    use 4096 keysize for digicert

--- deploytools/DigitCertAndJarSign/genCsr.bat
+++ deploytools/DigitCertAndJarSign/genCsr.bat
@@ -1,3 +1,3 @@
-keytool -genkey -alias sia -keyalg RSA -keysize 2048 -keystore sia.jks -dname "CN=Sports Information Analysis LLC,OU=IT, O=Sports Information Analysis LLC, L=Manalapan , ST=NJ,C=USA"  < siakeystorepass
+keytool -genkey -alias sia -keyalg RSA -keysize 4096 -keystore sia.jks -dname "CN=Sports Information Analysis LLC,OU=IT, O=Sports Information Analysis LLC, L=Manalapan , ST=NJ,C=USA"  < siakeystorepass
 keytool -certreq -alias sia -file sia.csr -keystore sia.jks < siakeystorepass
 echo copy sia.csr to digicit
