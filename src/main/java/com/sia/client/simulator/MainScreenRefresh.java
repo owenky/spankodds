64d96cc1890d0737a991de6741a4bf3635c7a0b9
Frank zheng <smldragon@hotmail.com>
2021-11-01 21:16:28 -0400

    add log to file feature

--- src/main/java/com/sia/client/simulator/MainScreenRefresh.java
+++ src/main/java/com/sia/client/simulator/MainScreenRefresh.java
@@ -1,6 +1,5 @@
 package com.sia.client.simulator;
 
-import com.sia.client.model.SportType;
 import com.sia.client.ui.control.MainScreen;
 import com.sia.client.ui.control.SportsTabPane;
 
@@ -13,16 +12,17 @@ public class MainScreenRefresh extends TestExecutor{
     private final MainScreen mainScreen;
     private static final AtomicInteger counter = new AtomicInteger(0);
     public MainScreenRefresh(MainScreen mainScreen) {
-        super(5,15);
+        super(15,20);
         this.mainScreen = mainScreen;
-        setValid(mainScreen.getName().equals(SportType.Soccer.getSportName()));
+//        setValid(mainScreen.getName().equals(SportType.Soccer.getSportName()));
+        setValid(true);
     }
     @Override
     public void run() {
 
         try {
             if(0==counter.getAndAdd(1)%5) {
-                log("***** MainScreen Refreshing count=" + counter.get());
+                log("***** Refreshing MainScreen "+mainScreen.getSportType().getSportName()+", count=" + counter.get());
             }
             SportsTabPane.refreshMainScreen(mainScreen);
         } catch(Exception e) {
