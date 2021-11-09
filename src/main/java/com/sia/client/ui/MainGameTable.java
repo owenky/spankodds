dd3733442e68aabe621daa925a7a9e5f1a933012
Frank zheng <smldragon@hotmail.com>
2021-11-09 13:09:39 -0500

    modified how row height is calculated for customized sport

--- src/main/java/com/sia/client/ui/MainGameTable.java
+++ src/main/java/com/sia/client/ui/MainGameTable.java
@@ -4,19 +4,19 @@ import com.sia.client.config.SiaConst;
 import com.sia.client.model.Game;
 import com.sia.client.model.LinesTableDataSupplier;
 import com.sia.client.model.MainGameTableModel;
+import com.sia.client.model.SportType;
 
 import javax.swing.table.TableCellRenderer;
-import java.util.HashMap;
-import java.util.Map;
 
 public class MainGameTable extends ColumnCustomizableTable<Game> implements LinesTableDataSupplier {
 
     private final LineRenderer soccerLineRenderer = new LineRenderer(SiaConst.SoccerStr);
     private final LineRenderer lineRenderer = new LineRenderer();
-    private final Map<Integer, Boolean> rowToSoccerStatusMap = new HashMap<>();
+    private final SportType sporetType;
 
     public MainGameTable(MainGameTableModel tm) {
         super(false,tm);
+        sporetType = tm.getSportType();
     }
     @Override
     protected RowHeaderGameTable createNewRowHeaderTable() {
@@ -28,12 +28,7 @@ public class MainGameTable extends ColumnCustomizableTable<Game> implements Line
     }
     @Override
     public TableCellRenderer getUserCellRenderer(int rowViewIndex, int colViewIndex) {
-        Boolean isSoccer = rowToSoccerStatusMap.get(rowViewIndex);
-        if ( null != isSoccer && isSoccer) {
-            return soccerLineRenderer;
-        } else {
-            return lineRenderer;
-        }
+         return isSoccer(rowViewIndex)? soccerLineRenderer:lineRenderer;
     }
     @Override
     public LinesTableData getLinesTableData(int row) {
@@ -46,17 +41,25 @@ public class MainGameTable extends ColumnCustomizableTable<Game> implements Line
     @Override
     public void configHeaderRow() {
         super.configHeaderRow();
-        if ( null != rowToSoccerStatusMap) {
-            rowToSoccerStatusMap.clear();
-            for (int rowViewIndex = 0; rowViewIndex < getRowCount(); rowViewIndex++) {
-                rowToSoccerStatusMap.put(rowViewIndex, isSoccer(rowViewIndex));
-            }
+        if ( ! getModel().getSportType().isPredifined()) {
+            //for customized sport, stage table section contains mixed sport type games.
+            //row height has to be calculated per row rather than per section -- 2021-11-09
+           for(int rowViewIndex=0;rowViewIndex < getRowCount();rowViewIndex++) {
+               if ( isSoccer(rowViewIndex)) {
+                   setRowHeight(rowViewIndex,SiaConst.SoccerRowheight);
+               }
+           }
         }
     }
 //    public LinesTableData findTableSectionByHeaderValue(GameGroupHeader gameGroupHeader) {
 //        return getModel().findTableSectionByHeaderValue(gameGroupHeader);
 //    }
     private boolean isSoccer(int rowViewIndex) {
+        if ( sporetType.equals(SportType.Soccer)) {
+            return true;
+        } else if (sporetType.isPredifined()) {
+            return false;
+        }
         int rowModelIndex = this.convertRowIndexToModel(rowViewIndex);
         Game g  = getModel().getGame(rowModelIndex);
         return SiaConst.SoccerLeagueId == g.getLeague_id();
