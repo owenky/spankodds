aafc1fee9eb12163d602783618b58fb31ce088f8
Frank Zheng <smldragon@hotmail.com>
2022-06-01 10:05:45 -0400

    support LineSeekerNode

--- src/main/java/com/sia/client/ui/AppController.java
+++ src/main/java/com/sia/client/ui/AppController.java
@@ -28,7 +28,6 @@ public class AppController {
     private static final AtomicReference<CountDownLatch> messageProcessingLatchRef = new AtomicReference<>(new CountDownLatch(1));
     public static Vector<String> customTabsVec = new Vector<>();
     public static Vector<LineAlertNode> linealertnodes = new Vector<>();
-    public static Vector<LineSeekerNode> lineseekernodes = new Vector<>();
     public static Vector<SportsMenuBar> menubars = new Vector<>();
     public static Vector<Sport> sportsVec = new Vector<>();
     public static String brokerURL = "failover:(tcp://71.172.25.164:61616)";
@@ -530,19 +529,12 @@ public class AppController {
         }
         return nwa;
     }
-
     public static Vector<String> getCustomTabsVec() {
         return customTabsVec;
     }
-
     public static Vector<LineAlertNode> getLineAlertNodes() {
         return linealertnodes;
     }
-
-    public static Vector<LineSeekerNode> getLineSeekerNodes() {
-        return lineseekernodes;
-    }
-
     public static SportsTabPane getMainTabPane() {
         return SpankyWindow.getFirstSpankyWindow().getSportsTabPane();
     }
