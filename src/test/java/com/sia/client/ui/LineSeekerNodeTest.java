c2384ee3a4a2c9a2c7722af741ff2cdab7b821aa
Frank Zheng <smldragon@hotmail.com>
2022-06-01 23:46:10 -0400

    add unit test

---
+++ src/test/java/com/sia/client/ui/LineSeekerNodeTest.java
@@ -0,0 +1,37 @@
+package com.sia.client.ui;
+
+import com.sia.client.config.Config;
+import com.sia.client.ui.lineseeker.*;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+import static org.junit.Assert.assertEquals;
+
+public class LineSeekerNodeTest {
+
+    private AlertPeriod period = AlertPeriod.Full;
+    private int gameId = 1;
+    private String key = gameId + Config.KeyJointer+period.name();
+
+    @Before
+    public void init() {
+        AlertConfig alertConfig = AlertAttrManager.of(gameId,period);
+        LineSeekerAttribute lineSeekerAttribute = alertConfig.getSectionAtrribute(AlertSectionName.totalsName);
+        lineSeekerAttribute.getHomeColumn().setLineInput("1");
+        lineSeekerAttribute.getHomeColumn().setJuiceInput("2");
+
+    }
+    @Test
+    public void testOf() {
+        List<LineSeekerNode> nodes = LineSeekerNode.getAllNodes();
+
+        assertEquals(1,nodes.size());
+
+        LineSeekerNode node = nodes.get(0);
+        assertEquals(1,node.get)
+    }
+}
